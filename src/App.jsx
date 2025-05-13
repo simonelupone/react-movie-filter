60ea629b4eec631235443240d3568e02cc16cd22
simonelupone <simone.lupone@gmail.com>
2025-05-13 20:54:19 +0200

    feat: add movies list and genres select

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import {useState} from "react";
+
+const initialMovies = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
 const App = () => {
+
+    const [genre, setGenre] = useState('');
+
     return (
         <div>
-            <h1 className='text-red-500'>Hello World!</h1>
+            <select
+                value={genre}
+                onChange={(e) => setGenre(e.target.value)}
+                name="movies" id="movies">
+                <option value="">Generi</option>
+                <option value="fantascienza">Fantascienza</option>
+                <option value="thriller">Thriller</option>
+                <option value="romantico">Romantico</option>
+                <option value="azione">Azione</option>
+            </select>
+
+            <ul>
+                {initialMovies.map(movie => <li>{movie.title}</li>)}
+            </ul>
         </div>
     );
 };
